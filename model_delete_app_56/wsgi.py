afbca70a4c26056f7c61d07f0fde1acf0e7eb3ad
Shah Rukh Khan <shah@crowdbotics.com>
2020-10-08 08:09:40 +0000

    Initial commit.

---
+++ model_delete_app_56/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for model_delete_app_56 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_delete_app_56.settings')
+
+application = get_wsgi_application()
